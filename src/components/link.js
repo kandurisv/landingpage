475c398bdc4eff378ae6d30da54580b889bb8157
kandurisv <30374472+kandurisv@users.noreply.github.com>
2022-01-31 21:34:38 +0530

    hindi zindabad

--- src/components/link.js
+++ src/components/link.js
@@ -1,7 +1,7 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx, NavLink as MenuLink, Link as A } from 'theme-ui';
-import NextLink from 'next/link';
+import { jsx, NavLink as MenuLink, Link as A } from "theme-ui";
+import NextLink from "next/link";
 export function NavLink({ path, label, children, ...rest }) {
   return (
     <NextLink href={path}>
