943067aa15ad15ab420e32c68b1b9955a18aa301
kandurisv <30374472+kandurisv@users.noreply.github.com>
2022-02-03 18:50:23 +0530

    analytics added to variant

--- pages/v1.js
+++ pages/v1.js
@@ -14,8 +14,25 @@ import Pricing from "sections/pricing";
 import ProductFeature from "sections/product-feature";
 
 import Banner1 from "sections/banner1";
+import { useRouter } from "next/router";
 
 export default function V1() {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <ThemeProvider theme={theme}>
       <StickyProvider>
